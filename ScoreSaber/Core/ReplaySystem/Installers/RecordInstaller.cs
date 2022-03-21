f7065849404b99d9c53eafa9e52283ce60d88e3c
Auros Nexus <aurosnex@gmail.com>
2022-03-21 05:39:47 -0400

    Disable Recordings for slider events

--- ScoreSaber/Core/ReplaySystem/Installers/RecordInstaller.cs
+++ ScoreSaber/Core/ReplaySystem/Installers/RecordInstaller.cs
@@ -1,11 +1,28 @@
 ﻿using Zenject;
 using ScoreSaber.Core.ReplaySystem.Recorders;
+using SiraUtil.Logging;
 
 namespace ScoreSaber.Core.ReplaySystem.Installers
 {
     internal class RecordInstaller : Installer
     {
+        private readonly SiraLog _siraLog;
+        private readonly GameplayCoreSceneSetupData _gameplayCoreSceneSetupData;
+
+        public RecordInstaller(SiraLog siraLog, GameplayCoreSceneSetupData gameplayCoreSceneSetupData) {
+
+            _siraLog = siraLog;
+            _gameplayCoreSceneSetupData = gameplayCoreSceneSetupData;
+        }
         public override void InstallBindings() {
+
+            bool hasV3Stuff = ContainsV3Stuff();
+
+            if (hasV3Stuff) {
+
+                _siraLog.Warn("This map contains Beatmap V3 sliders! Not recording...");
+                return;
+            }
 
             if (!Plugin.ReplayState.isPlaybackEnabled) {
 
@@ -17,6 +34,14 @@ namespace ScoreSaber.Core.ReplaySystem.Installers
                 Container.BindInterfacesAndSelfTo<ScoreEventRecorder>().AsSingle();
                 Container.BindInterfacesAndSelfTo<EnergyEventRecorder>().AsSingle();
             }
+        }
+
+        private bool ContainsV3Stuff() {
+
+            foreach (var item in _gameplayCoreSceneSetupData.transformedBeatmapData.allBeatmapDataItems)
+                if (item.type == BeatmapDataItem.BeatmapDataItemType.BeatmapObject && item is SliderData)
+                    return true;
+            return false;
         }
     }
 }
