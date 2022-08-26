3e3e610fef17b90829059c13798006307609f496
Auros Nexus <41306347+Auros@users.noreply.github.com>
2022-08-26 17:59:14 -0400

    Added repacking and source generators

---
+++ ScoreSaber/UI/NotifyTest.cs
@@ -0,0 +1,9 @@
+﻿using PropertyChanged.SourceGenerator;
+
+namespace ScoreSaber.UI
+{
+    internal partial class NotifyTest : ScoreSaberViewController
+    {
+        [Notify] private string _hello;
+    }
+}
---
+++ ScoreSaber/UI/ScoreSaberViewController.cs
@@ -0,0 +1,9 @@
+﻿using BeatSaberMarkupLanguage.ViewControllers;
+
+namespace ScoreSaber.UI
+{
+    internal class ScoreSaberViewController : BSMLAutomaticViewController
+    {
+        protected new virtual void NotifyPropertyChanged(string name) => base.NotifyPropertyChanged(name);
+    }
+}
