f64c6130aefa8a6ed80601c1834d6d1d18660cb6
Auros Nexus <41306347+Auros@users.noreply.github.com>
2022-11-30 07:26:20 -0500

    Loading indicator for Score Table

--- ScoreSaber/UI/Menu/Hosts/ScoreTableHost.cs
+++ ScoreSaber/UI/Menu/Hosts/ScoreTableHost.cs
@@ -13,4 +13,10 @@ internal sealed partial class ScoreTableHost
     [Notify]
     [UsedImplicitly]
     private List<ScoreTableCell> _cells = Enumerable.Range(0, MaximumCellCount).Select(_ => new ScoreTableCell()).ToList();
+
+    [Notify]
+    [AlsoNotify(nameof(Loaded))]
+    private bool _loading;
+
+    public bool Loaded => !Loading;
 }
