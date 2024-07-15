df35744d0f8c2fc07559c527a5451ed603400c8f
Qwasyx <qwasyx3000@gmail.com>
2024-07-15 13:08:09 +0200

    update to 1.37.1

--- ScoreSaber/Core/Utils/MaxScoreCache.cs
+++ ScoreSaber/Core/Utils/MaxScoreCache.cs
@@ -9,13 +9,15 @@ namespace ScoreSaber.Core.Utils {
     internal class MaxScoreCache {
         private readonly BeatmapLevelLoader _beatmapLevelLoader;
         private readonly BeatmapDataLoader _beatmapDataLoader;
+        private readonly BeatmapLevelsEntitlementModel _beatmapLevelsEntitlementModel;
  
 
         private Dictionary<BeatmapKey, int> cache = new Dictionary<BeatmapKey, int>();
 
-        public MaxScoreCache(BeatmapLevelLoader beatmapLevelLoader, BeatmapDataLoader beatmapDataLoader) {
+        public MaxScoreCache(BeatmapLevelLoader beatmapLevelLoader, BeatmapDataLoader beatmapDataLoader, BeatmapLevelsEntitlementModel beatmapLevelsEntitlementModel) {
             _beatmapLevelLoader = beatmapLevelLoader;
             _beatmapDataLoader = beatmapDataLoader;
+            _beatmapLevelsEntitlementModel = beatmapLevelsEntitlementModel;
         }
 
         public async Task<int> GetMaxScore(BeatmapLevel beatmapLevel, BeatmapKey beatmapKey) {
@@ -23,8 +25,17 @@ namespace ScoreSaber.Core.Utils {
                 return cache[beatmapKey];
             }
 
-            var beatmapLevelData = (await _beatmapLevelLoader.LoadBeatmapLevelDataAsync(beatmapLevel, CancellationToken.None)).beatmapLevelData;
-            var beatmapData = await _beatmapDataLoader.LoadBeatmapDataAsync(beatmapLevelData, beatmapKey, beatmapLevel.beatsPerMinute, false, null, null, null, false);
+            var beatmapLevelDataVersion = await _beatmapLevelsEntitlementModel.GetLevelDataVersionAsync(beatmapKey.levelId, CancellationToken.None);
+            var beatmapLevelData = (await _beatmapLevelLoader.LoadBeatmapLevelDataAsync(beatmapLevel, beatmapLevelDataVersion, CancellationToken.None)).beatmapLevelData;
+            var beatmapData = await _beatmapDataLoader.LoadBeatmapDataAsync(beatmapLevelData: beatmapLevelData,
+                                                                            beatmapKey: beatmapKey,
+                                                                            startBpm: beatmapLevel.beatsPerMinute,
+                                                                            loadingForDesignatedEnvironment: false,
+                                                                            environmentInfo: null,
+                                                                            beatmapLevelDataVersion: beatmapLevelDataVersion,
+                                                                            gameplayModifiers: null,
+                                                                            playerSpecificSettings: null,
+                                                                            enableBeatmapDataCaching: false);
             int maxScore = ScoreModel.ComputeMaxMultipliedScoreForBeatmap(beatmapData);
 
             cache[beatmapKey] = maxScore;
