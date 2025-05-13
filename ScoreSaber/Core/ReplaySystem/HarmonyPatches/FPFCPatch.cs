5ae1b314c0faa6b77093bf57173b6bc40ae632ba
Qwasyx <qwasyx3000@gmail.com>
2025-05-13 20:52:42 +0200

    fix pause at start of replay when FPFC is enabled

--- ScoreSaber/Core/ReplaySystem/HarmonyPatches/FPFCPatch.cs
+++ ScoreSaber/Core/ReplaySystem/HarmonyPatches/FPFCPatch.cs
@@ -5,17 +5,23 @@ namespace ScoreSaber.Core.ReplaySystem.HarmonyPatches {
     [Bind]
     internal class FPFCPatch : IAffinity {
 
-        private readonly bool _isOculus;
+        private readonly bool _needsPatching;
         private readonly IVRPlatformHelper _vrPlatformHelper;
 
         public FPFCPatch(IVRPlatformHelper vrPlatformHelper) {
             _vrPlatformHelper = vrPlatformHelper;
-            _isOculus = _vrPlatformHelper is OculusVRHelper;
+            _needsPatching = _vrPlatformHelper is OculusVRHelper || _vrPlatformHelper is UnityXRHelper;
         }
 
         [AffinityPatch(typeof(OculusVRHelper), nameof(OculusVRHelper.hasInputFocus), AffinityMethodType.Getter)]
-        protected void ForceInputFocus(ref bool __result) {
-            if (_isOculus && Plugin.ReplayState.IsPlaybackEnabled)
+        protected void ForceInputFocusOculusVR(ref bool __result) {
+            if (_needsPatching && Plugin.ReplayState.IsPlaybackEnabled)
+                __result = true;
+        }
+
+        [AffinityPatch(typeof(UnityXRHelper), nameof(UnityXRHelper.hasInputFocus), AffinityMethodType.Getter)]
+        protected void ForceInputFocusUnityXR(ref bool __result) {
+            if (_needsPatching && Plugin.ReplayState.IsPlaybackEnabled)
                 __result = true;
         }
     }
